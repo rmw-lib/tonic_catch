be70dd7ec9fde268bf5b1f328e06491164363dc7
gcxfd <i@rmw.link>
2022-01-13 12:29:41 +0800

    .

---
+++ proc/clippy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# 检测坏代码风格
+
+set -e
+
+_DIR=$(dirname $(realpath "$0"))
+
+cd $_DIR
+
+
+if ! hash cargo-clippy 2>/dev/null; then
+  rustup component add clippy
+fi
+
+gsync
+
+cargo +nightly clippy --fix -Z unstable-options --broken-code
+cargo fmt
+
