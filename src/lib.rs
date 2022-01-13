715b438404d48a2b6d40cfda8bcc4d604b8290b4
gcxfd <i@rmw.link>
2022-01-13 11:09:24 +0800

    🔶

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-use proc_macro::TokenStream;
+mod parse;
+
 use quote::quote;
+use proc_macro::TokenStream;
 use syn::parse_macro_input;
+use crate::parse::Item;
 
 #[proc_macro_attribute]
 pub fn tonic_catch(_: TokenStream, input: TokenStream) -> TokenStream {
+  let mut item = parse_macro_input!(input as Item);
+
   println!("{:?}", input);
   input
 }
